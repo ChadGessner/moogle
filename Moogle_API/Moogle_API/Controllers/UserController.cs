fea4567371be97e91e1607714c985196c25f1620
Chad Gessner <CGess83@me.com>
2023-03-14 16:12:27 -0400

    Got rid of forecast controller and added user controller

---
+++ Moogle_API/Moogle_API/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Moogle_API.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class TheaterController : ControllerBase
+  {
+
+  }
+}

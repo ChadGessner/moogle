bd20dfab2d441644e6e59bc7a22f6cecab26b4f2
unknown <cjsteward07@gmail.com>
2023-03-15 20:17:03 -0400

    removing comment

--- Moogle_API/Moogle_Models/API_Models/News/NewsData.cs
+++ Moogle_API/Moogle_Models/API_Models/News/NewsData.cs
@@ -6,7 +6,6 @@ using System.Threading.Tasks;
 
 namespace Moogle_Models.API_Models.News
 {
-  // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
   public class NewsData
   {
     public List<NewsStory> newsStories { get; set; }

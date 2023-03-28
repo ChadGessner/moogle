7573fffc12de217976e9e4d5360a97f5f1b18e78
cjsteward07 <112128824+cjsteward07@users.noreply.github.com>
2023-03-28 16:47:20 -0400

    Update FlixterClient.cs

--- Moogle_API/Moogle_Flixter_Domain/FlixterClient.cs
+++ Moogle_API/Moogle_Flixter_Domain/FlixterClient.cs
@@ -105,7 +105,7 @@ namespace Moogle_Flixter_Domain
       string apiUri = BaseUri + "news/list";
       var header = apiUri
           .WithHeader("X-RapidAPI-Key", "f126303238mshb0ca04fe3d5056ep12846bjsncbdcff9eea5d")
-        .WithHeader("X-RapidAPI-Key", "a5eb94d850msh3000c463db4a25dp1f3109jsn756a961153bc")
+        //.WithHeader("X-RapidAPI-Key", "a5eb94d850msh3000c463db4a25dp1f3109jsn756a961153bc")
         //.WithHeader("X-RapidAPI-Key", "8a1920a098mshfc90a10a8464b5ap1cf15ejsndf3b1770a944")
           //.WithHeader("X-RapidAPI-Key", "52b17fc647msh1f76838d7d2fa42p1bdf0cjsnd8db47c39b4b")
           .WithHeader("X-RapidAPI-Host", "flixster.p.rapidapi.com");

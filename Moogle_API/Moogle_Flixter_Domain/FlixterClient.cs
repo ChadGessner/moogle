a7a490d788dc1445a3a3ed6421b1fd811258304b
ChadGessner <108926447+ChadGessner@users.noreply.github.com>
2023-04-03 07:55:16 -0400

    making commit so I can pull

--- Moogle_API/Moogle_Flixter_Domain/FlixterClient.cs
+++ Moogle_API/Moogle_Flixter_Domain/FlixterClient.cs
@@ -20,7 +20,7 @@ namespace Moogle_Flixter_Domain
     public string BaseUri { get; set; } = "https://flixster.p.rapidapi.com/";
     public string TestUri { get; set; } = @"C:\Users\Chad\Desktop\GC_Angular\moogle-app\Moogle_API\Moogle_Flixter_Domain\ModelsTest.json";
     public string Headers { get; set; }
-    public string ApiKey { get; set; } = "102d0b565cmshc853115aca2193ap1bb28bjsn8bbf500ac940";
+    public string ApiKey { get; set; } = "86b7ccfa8fmsh4fca1ada0d0bf38p1dfee7jsn3fe8d10a991a";
 
     public FlixterClient()
     {
